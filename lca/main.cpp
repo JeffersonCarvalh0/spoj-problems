0c715f9d1bba6515c2aa24d3bf98407b0b408ce9
Jefferson <jeffersonhcarvalho@gmail.com>
2018-09-03 10:02:38 -0300

    Tried to redo lca, now it gets RTE

--- lca/main.cpp
+++ lca/main.cpp
@@ -1,78 +1,94 @@
-// WA
+// RTE
 
 # include <iostream>
-# include <cstring>
 # include <vector>
 # include <list>
 
-# define MAXN 1005
-# define SZ 2005
-
 using namespace std;
 
-int first[MAXN], visited[MAXN], st[SZ][10], logs[SZ], t, n, m, cur, sz, q, v, w;
-vector<list<int>> adj_list;
-vector<int> euler, heights;
+int logs[2005];
 
-void dfs(int v = 0, int h = 1) {
-    visited[v] = true;
-    euler.push_back(v);
-    heights.push_back(h);
-    first[v] = euler.size() - 1;
+class SparseTable {
+public:
+    vector<vector<int>> st;
+    vector<int> &a;
 
-    for (auto &adj : adj_list[v]) {
-        if (!visited[adj]) {
-            dfs(adj, h + 1);
-            euler.push_back(v);
-            heights.push_back(h);
-        }
-    }
-}
-
-int main() {
-    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-
-    logs[1] = 0;
-    for (int i = 2; i < SZ; ++i) logs[i] = logs[i/2] + 1;
-
-    cin >> t;
-
-    for (int c = 1; c <= t; ++c) {
-        cout << "Case " << c << ":\n";
-        cin >> n;
-        sz = (n * 2) - 1;
-
-        adj_list.clear(); adj_list.resize(n);
-        for (int i = 0; i < n; ++i) {
-            cin >> m;
-            for (int j = 0; j < m; ++j) {
-                cin >> cur;
-                adj_list[i].push_back(--cur);
-                adj_list[cur].push_back(i);
-            }
-        }
-
-        euler.clear(); heights.clear();
-        memset(visited, 0, sizeof(visited));
-        memset(first, sz + 5, sizeof(first));
-        dfs();
-
-        for (int i = 0; i < sz; ++i) st[i][0] = i;
-        for (int j = 1; j <= logs[sz]; ++j) {
-            for (int i = 0; i + (1 << j) <= sz; ++i) {
-                if (heights[st[i][j - 1]] < heights[st[i + (1 << (j - 1))][j - 1]])
+    SparseTable(vector<int> &a): a(a) {
+        int n = a.size();
+        st = vector<vector<int>>(n, vector<int>(logs[n]));
+        for (int i = 0; i < n; ++i) st[i][0] = i;
+        for (int j = 1; j <= logs[n]; ++j) {
+            for (int i = 0; i + (1 << j) <= n; ++i) {
+                if (a[st[i][j - 1]] < a[st[i + (1 << (j - 1))][j - 1]])
                     st[i][j] = st[i][j - 1];
                 else
                     st[i][j] = st[i + (1 << (j - 1))][j - 1];
             }
         }
+    }
+
+    int query(int l, int r) {
+        if (r < l) swap(l, r);
+        int span = r - l + 1, j = logs[span];
+        if (a[st[l][j]] < a[st[l + span - (1 << j)][j]])
+            return st[l][j];
+        else
+            return st[l + span - (1 << j)][j];
+    }
+};
+
+class LCA {
+public:
+    vector<int> euler, heights, first;
+    vector<bool> visited;
+
+    LCA(vector<list<int>> &adj_list) {
+        visited.resize(adj_list.size(), false);
+        first.resize(adj_list.size());
+        dfs(0, 1, adj_list);
+    }
+
+    void dfs(int v, int h, vector<list<int>> &adj_list) {
+        visited[v] = true;
+        first[v] = euler.size(); euler.push_back(v); heights.push_back(h);
+
+        for (auto &adj : adj_list[v]) {
+            if (!visited[adj])
+                dfs(adj, h + 1, adj_list), euler.push_back(v), heights.push_back(h);
+        }
+    }
+
+    int query(int v1, int v2, SparseTable &st) {
+        return euler[st.query(first[v1], first[v2])];
+    }
+};
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int t, n, m, cur, q, v, w;
+
+    logs[1] = 0;
+    for (int i = 2; i < 2005; ++i) logs[i] = logs[i/2] + 1;
+
+    cin >> t;
+    for (int c = 1; c <= t; ++c) {
+        cout << "Case: " << c << ":\n";
+
+        cin >> n;
+        vector<list<int>> adj_list(n);
+        for (int i = 0; i < n; ++i) {
+            cin >> m;
+            while (m--)
+                cin >> cur, adj_list[i].push_back(--cur), adj_list[cur].push_back(i);
+        }
+
+        LCA lca(adj_list);
+        SparseTable st(lca.heights);
 
         cin >> q;
         while (q--) {
-            cin >> v >> w; v = first[v - 1]; w = first[w - 1];
-            if (w < v) swap(v, w);
-            int span = w - v + 1, j = logs[span];
-            cout << euler[min(st[v][j], st[v + span - (1 << j)][j])] + 1 << '\n';
+            cin >> v >> w; --v; --w;
+            cout << lca.query(v, w, st) + 1 << '\n';
         }
     }
 
